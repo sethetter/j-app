f8c83c60b7cbc2682c684fbc84112db8ef571d72
Seth Etter <sethetter@gmail.com>
2013-09-29 10:53:00 -0500

    Moved pass encryption to beforeCreate on User

--- api/models/User.js
+++ api/models/User.js
@@ -13,6 +13,21 @@ module.exports = {
     email    : 'string',
     password : 'string'
     
+  },
+
+  beforeCreate: function (attrs, next) {
+    var bcrypt = require('bcrypt');
+
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(attrs.password, salt, function(err, hash) {
+        if (err) return next(err);
+
+        attrs.password = hash;
+        next();
+      });
+    });
   }
 
 };
