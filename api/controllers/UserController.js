f9b0cebe9291d843ace6c797a1f6a3bd152fdec3
Seth Etter <sethetter@gmail.com>
2013-09-29 12:24:49 -0500

    Login method for session

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,11 +7,33 @@
 
 module.exports = {
 
-  /* e.g.
-  sayHello: function (req, res) {
-    res.send('hello world!');
+
+  login: function (req, res) {
+    var bcrypt = require('bcrypt');
+
+    User.findOneByEmail(req.body.email).done(function (err, user) {
+      //if (err) res.json({ error: 'DB error' }, 500);
+      if (err) res.json(err, 500);
+
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, function (err, match) {
+          if (err) res.json({ error: 'Server error' }, 500);
+
+          if (match) {
+            // password match
+            req.session.user = user.id;
+            res.json(user);
+          } else {
+            // invalid password
+            if (req.session.user) req.session.user = null;
+            res.json({ error: 'Invalid password' }, 400);
+          }
+        });
+      } else {
+        res.json({ error: 'User not found' }, 404);
+      }
+    });
   }
-  */
   
 
 };
